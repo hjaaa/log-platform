78e8573ed57c9bb18abd3d7cbf94cf674478bb75
huangjian <hj19961223@gmail.com>
2026-07-04 10:24:17 +0800

    chore: 移除骨架工具层(.claude/context/scripts)与 _archive 归档代码

--- _archive/log-core-skeleton/src/test/java/com/hj/log/CoreTestApplication.java
+++
@@ -1,7 +0,0 @@
-package com.hj.log;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages = "com.hj.log")
-public class CoreTestApplication {
-}
